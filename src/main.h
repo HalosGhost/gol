f63bd211ef0187fdad83b7f67c0826bf2f88c167
halosghost <samuel.stuewe@gmail.com>
2019-09-29 20:43:38 -0500

    move option declarations into an X macro

--- src/main.h
+++ src/main.h
@@ -32,11 +32,15 @@ static size_t cells;
 static volatile sig_atomic_t run_state;
 static volatile sig_atomic_t caught_signum;
 
+#define FOR_EACH_OPTION \
+    X(random, 1) \
+    X(empty,  0) \
+    X(pause,  1) \
+    X(help,   0)
+
+#define X(longopt, arg) { #longopt, arg, 0, (#longopt)[0] },
 static struct option os[] = {
-    { "random", 1, 0, 'r' },
-    { "empty",  0, 0, 'e' },
-    { "pause",  1, 0, 'p' },
-    { "help",   0, 0, 'h' },
+    FOR_EACH_OPTION
     { 0, 0, 0, 0 },
 };
-
+#undef X
